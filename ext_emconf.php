4b37afffc7986635c4e1116e06b79078fab5a9aa
ende <sascha@sascha-ende.de>
2018-03-09 23:29:08 +0100

    docs, version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'T3Helpers',
-    'description' => 'Helpers like in LARAVEL: Simple and easy functions that make your TYPO3 life with extbase and extension development a little easier.',
+    'description' => 'Helpers like in LARAVEL: Simple and easy functions that make your TYPO3 life with extbase and extension development a little easier. IMPORTANT: During BETA everything can change. Please wait some days until the final stable version is ready to use.',
     'category' => 'tools',
     'author' => 'Sascha Ende',
     'author_email' => 'sascha@sascha-ende.de',
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.9.0',
+    'version' => '0.9.1',
     'constraints' => [
         'depends' => [
             'typo3' => '8.6.0-8.7.99',
