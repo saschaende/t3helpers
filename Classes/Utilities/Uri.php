3d685379c2ae89be43bf2505fa4b795a15590392
Sascha Ende <sascha@ende.us>
2018-03-20 13:12:45 +0100

    use links also in the Backend

--- Classes/Utilities/Uri.php
+++ Classes/Utilities/Uri.php
@@ -2,7 +2,10 @@
 
 namespace SaschaEnde\T3helpers\Utilities;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class Uri
@@ -10,6 +13,12 @@ use TYPO3\CMS\Core\SingletonInterface;
  * @package SaschaEnde\T3helpers\Utilities
  */
 class Uri implements SingletonInterface {
+
+    public function __construct() {
+        if(TYPO3_MODE == 'BE'){
+            $this->initTSFE();
+        }
+    }
 
     public function getByPid($pid, $useCacheHash = true, $forceAbsoluteUrl = true) {
         return $GLOBALS['TSFE']->cObj->typoLink_URL([
@@ -40,6 +49,31 @@ class Uri implements SingletonInterface {
             'returnLast' => 'url',
             'forceAbsoluteUrl' => $forceAbsoluteUrl
         ]);
+    }
+
+    /*
+    * Initialise tsfe
+    */
+    function initTSFE($id = 1, $typeNum = 0) {
+        if (!is_object($GLOBALS['TT'])) {
+            $GLOBALS['TT'] = new \TYPO3\CMS\Core\TimeTracker\NullTimeTracker;
+            $GLOBALS['TT']->start();
+        }
+        $GLOBALS['TSFE'] = GeneralUtility::makeInstance('TYPO3\\CMS\\Frontend\\Controller\\TypoScriptFrontendController', $GLOBALS['TYPO3_CONF_VARS'], $id, $typeNum);
+        $GLOBALS['TSFE']->connectToDB();
+        $GLOBALS['TSFE']->initFEuser();
+        $GLOBALS['TSFE']->determineId();
+        $GLOBALS['TSFE']->initTemplate();
+        $GLOBALS['TSFE']->getConfigArray();
+
+        $confManager = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Configuration\ConfigurationManager::class);
+        $GLOBALS['TSFE']->cObj = $confManager->getContentObject();
+
+        if (ExtensionManagementUtility::isLoaded('realurl')) {
+            $rootline = BackendUtility::BEgetRootLine($id);
+            $host = BackendUtility::firstDomainRecord($rootline);
+            $_SERVER['HTTP_HOST'] = $host;
+        }
     }
 
 
