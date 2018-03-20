3b5b4e089a0ac1628612ef26dff75ed579583d96
Sascha Ende <sascha@ende.us>
2018-03-20 15:02:27 +0100

    https -> curl

--- Classes/Utilities/Google.php
+++ Classes/Utilities/Google.php
@@ -7,7 +7,7 @@ use TYPO3\CMS\Core\SingletonInterface;
 class Google implements SingletonInterface {
 
     public function getGeoCoordinates($googleApiKey, $address) {
-        $res = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?key=' . $googleApiKey . '&address=' . $address);
+        $res = $this->file_get_contents_curl('https://maps.googleapis.com/maps/api/geocode/json?key=' . $googleApiKey . '&address=' . $address);
         $res = json_decode($res);
         if(isset($res->results[0]->geometry->location->lat)){
             return [
@@ -17,6 +17,23 @@ class Google implements SingletonInterface {
         }else{
             return false;
         }
+
+    }
+
+    private function file_get_contents_curl($url) {
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
 
     }
 }
