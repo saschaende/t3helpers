5bdc503e5076f237ca40e364e0436608859903e2
Sascha Ende <sascha@ende.us>
2018-09-11 09:24:46 +0200

    Seo

--- Classes/ViewHelpers/Page/SeoViewHelper.php
+++ Classes/ViewHelpers/Page/SeoViewHelper.php
@@ -1,40 +1,9 @@
 <?php
 
-namespace SaschaEnde\T3helpers\ViewHelpers;
+namespace SaschaEnde\T3helpers\ViewHelpers\Page;
 
-/***************************************************************
- * Copyright notice
- *
- * (c) 2016 Marc Horst <info@marc-horst.de>
- * All rights reserved
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- * Example
- * {namespace m=TYPO3\Fluidseo\ViewHelpers}
- *
- * @package TYPO3
- * @subpackage Fluidseo
- * @version
- */
-class FluidseoViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
-    /**
-     * Simple Fluid SEO Viewhelper
-     * @param string $tag
-     */
+class SeoViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
     public function render($tag) {
         switch ($tag) {
             case 'title':
