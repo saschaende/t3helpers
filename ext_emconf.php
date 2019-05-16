b4decb586d931e17bc1fea05260a3233918bc446
Sascha Ende <sascha@sascha-ende.de>
2019-05-16 16:05:06 +0200

    Modified   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'T3Helpers',
     'description' => 'Helpers for Extbase: Simple and easy functions that make your TYPO3 life with extbase and extension development a little easier. Please let me know if you have any ideas or if you find any errors, i will fix this immediately.',
-    'category' => 'tools',
+    'category' => 'services',
     'author' => 'Filmmusic.io',
     'author_email' => 'info@filmmusic.io',
     'state' => 'stable',
