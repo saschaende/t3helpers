7e931e22d261b150c6846428aa67f45f24a28edc
Sascha Ende <sascha@sascha-ende.de>
2019-06-04 12:54:02 +0200

    Symfony components

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,13 +7,11 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$composerAutoloadFile = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)
-    . 'Resources/Private/Libraries/t3helpers.php';
-
-require_once($composerAutoloadFile);
+require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Private/Libraries/t3helpers.php');
+require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Private/vendor/autoload.php');
 
 // eID Dispatcher
-$TYPO3_CONF_VARS['FE']['eID_include']['t3h_example'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('t3helpers').'Classes/Examples/EidExample.php';
+$TYPO3_CONF_VARS['FE']['eID_include']['t3h_example'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('t3helpers') . 'Classes/Examples/EidExample.php';
 
 // Add viewhelper namespace
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['t3h'] = ['SaschaEnde\\T3helpers\\ViewHelpers'];
