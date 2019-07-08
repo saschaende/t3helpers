55e203040086a6ff94b8ad6d6c1cca15949e50d2
Sascha Ende <sascha@sascha-ende.de>
2019-07-08 16:35:57 +0200

    Modified   Classes/ViewHelpers/Css/ClassViewHelper.php

--- Classes/ViewHelpers/Css/ClassViewHelper.php
+++ Classes/ViewHelpers/Css/ClassViewHelper.php
@@ -2,28 +2,33 @@
 
 namespace SaschaEnde\T3helpers\ViewHelpers\Css;
 
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
 /**
  * {namespace t3h=SaschaEnde\T3helpers\ViewHelpers}
  */
-class ClassViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+class ClassViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
 
     /**
      * @return void
      */
-    public function initializeArguments() {
+    public function initializeArguments()
+    {
         $this->registerArgument('css', 'mixed', 'String to case format');
     }
 
-    public function render() {
+    public function render()
+    {
         $css = $this->arguments['css'];
-        if (is_array($css)) {
+        if(is_array($css)){
             $parts = [];
-            foreach ($css as $cssArrElement) {
-                $parts = array_merge($parts, explode(',', $cssArrElement));
+            foreach($css as $cssArrElement){
+                $parts = array_merge($parts,explode(',',$cssArrElement));
             }
-        } else {
-            $parts = explode(',', $css);
+        }else{
+            $parts = explode(',',$css);
         }
-        return implode(" ", $parts);
+        return implode(" ",$parts);
     }
 }
