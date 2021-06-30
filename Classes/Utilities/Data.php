d5590f4785150e629dd5e72e106928f76a40707a
Sascha <sascha@sascha-ende.de>
2021-07-01 00:18:00 +0200

    Modified   Classes/Utilities/Data.php

--- Classes/Utilities/Data.php
+++ Classes/Utilities/Data.php
@@ -332,4 +332,34 @@ class Data implements SingletonInterface {
         $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
         return strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $text));
     }
+
+    /**
+     * Generate a random string, using a cryptographically secure
+     * pseudorandom number generator (random_int)
+     *
+     * This function uses type hints now (PHP 7+ only), but it was originally
+     * written for PHP 5 as well.
+     *
+     * For PHP 7, random_int is a PHP core function
+     * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+     *
+     * @param int $length      How many characters do we want?
+     * @param string $keyspace A string of all possible characters
+     *                         to select from
+     * @return string
+     */
+    function random_str(
+        int $length = 64,
+        string $keyspace = '123456789abcdefghijklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
+    ): string {
+        if ($length < 1) {
+            throw new \RangeException("Length must be a positive integer");
+        }
+        $pieces = [];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
 }
