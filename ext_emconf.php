74830456217dd974a0829f3c72c884734864d930
Sascha Ende <sascha@ende.us>
2021-10-07 00:32:25 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,38 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'T3Helpers',
-    'description' => 'Helpers for Extbase: Simple and easy functions that make your TYPO3 life with extbase and extension development a little easier. Please let me know if you have any ideas or if you find any errors, i will fix this immediately.',
-    'category' => 'services',
-    'author' => 'Filmmusic.io UG (haftungsbeschränkt)',
-    'state' => 'stable',
-    'internal' => '',
-    'uploadfolder' => '0',
-    'createDirs' => '',
-    'clearCacheOnLoad' => 0,
-    'version' => '0.9.37',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '8.6.0-9.5.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
-];
+/***************************************************************
+ * Extension Manager/Repository config file for ext "t3helpers".
+ *
+ * Auto generated 07-10-2021 00:14
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'T3Helpers',
+  'description' => 'Helpers for Extbase: Simple and easy functions that make your TYPO3 life with extbase and extension development a little easier. Please let me know if you have any ideas or if you find any errors, i will fix this immediately.',
+  'category' => 'services',
+  'version' => '0.9.38',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'clearcacheonload' => false,
+  'author' => 'Filmmusic.io UG (haftungsbeschränkt)',
+  'author_email' => NULL,
+  'author_company' => NULL,
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '11.0.0-11.5.0',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+);
+
